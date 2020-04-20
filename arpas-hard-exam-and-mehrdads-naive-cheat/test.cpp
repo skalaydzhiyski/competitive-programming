ef196123bf452b65bd92cf9a8f8e0b01a6875274
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-04-20 18:42:03 +0100

    fix previous commit message

--- arpas-hard-exam-and-mehrdads-naive-cheat/test.cpp
+++ arpas-hard-exam-and-mehrdads-naive-cheat/test.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 
 int main(void) {
-  // write your solution here
+  int digits[] = {8, 4, 2, 6};
+  int n, x = 1378;
+  scanf("%d", &n);
+  printf("%d", n==0 ? 1 : digits[(n-1)%4]);
   return 0;
 }
 
