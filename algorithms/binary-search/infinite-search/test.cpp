a52d9865e4c274ed69d9cf2ec1d3ad96355a50cb
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-05-01 13:02:10 +0100

    now sorting problems on algo and platform

---
+++ algorithms/binary-search/infinite-search/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  /* Solution for this problem consists of finding the interval 
+   * in which our target value lies using the numbers in the array
+   * i.e. -> [a[i], a[i]**2] until a[i]**2 becomes bigger than the target value
+   * and then run binary search in the given interval to get O(log(N)) running time
+  */
+  return 0;
+}
+
