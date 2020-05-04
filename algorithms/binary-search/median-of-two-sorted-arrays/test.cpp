b999c062056ffb5aaeda69acb8c5dca9ab54742b
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-05-04 19:06:28 +0100

    refactored medians of two arrays problem with more reasonable variable names

--- algorithms/binary-search/median-of-two-sorted-arrays/test.cpp
+++ algorithms/binary-search/median-of-two-sorted-arrays/test.cpp
@@ -1,49 +1,55 @@
 #include <iostream>
 #include <climits>
-using namespace std;
+// well that is the way things are here and now brother and we are still trying to make this work 
+using std::cout;
+using std::endl;
+using std::max;
+using std::min;
 
+// that is the way to go here brother
 double median_arr(int* a, int n) {
   return n%2 == 0 ? (a[n/2-1]+a[n/2])/2 : a[n/2];
 }
 
-double solve(int* a, int* b, int x, int y) {
+/* Goal of this algorithm is to find the optimal partition (m)
+ * where mx + my = the median of the A U B  
+ * such that Am-1 <= Bm && Bm-1 <= Am (in this case the median is
+ *  equal to (max(Am-1, Bm-1) + min(Am, Bm)) / 2 if even number of elements in the union A and B
+ *  or max(Am-1, Bm-1) if there are odd number of elements in the union fo A and B 
+ * */
+double solve(int* a, int* b, int n, int m) {
   // always make 'a' to be the smaller array of the two 
-  if (x > y) {
-    return solve(b, a, y, x);
+  if (n > m) {
+    return solve(a, b, m, n);
   }
+  /* the left and right are moved across ONLY the smaller array (a) since the 
+   * partition of the smaller array (ma) affects the partition of the larger array (mb) */
   int left = 0;
-  int right = x;
-  int N = x + y;
+  int right = n;
+  int N = n + m;
   while (left <= right) {
-    cout << "left: " << left << "\tright: " << right << endl;
+    int ma = (left + right) / 2;
+    int mb = (n + m + 1) / 2 - ma;
 
-    int partX = (left + right) / 2;
-    int partY = (x+y+1)/2 - partX;
+    /* This is the bounds checking for when we go over the number of elements of 'a'
+     * */
+    int maxleftA = ma == 0 ? INT_MIN : a[ma-1];
+    int minrightA = ma == n ? INT_MAX : a[ma];
+    int maxleftB = mb == 0 ? INT_MIN : b[mb-1];
+    int minrightB = mb == m ? INT_MAX : b[mb];
 
-    cout << "partitions" << endl;
-    cout << partX << ": [" << a[partX] << "], " << partY << ": [" << b[partY] << "]" << endl;
-
-    int maxleftX = partX == 0 ? INT_MIN : a[partX-1];
-    int minrightX = partX == x ? INT_MAX : a[partX];
-    cout << "maxleftX: " << maxleftX << ", " << "minrightX: " << minrightX << endl;
-
-    int maxleftY = partY == 0 ? INT_MIN : b[partY-1];
-    int minrightY = partY == y ? INT_MAX : b[partY];
-    cout << "maxleftY: " << maxleftY << ", " << "minrightY: " << minrightY << endl;
-    cout << endl;
+    cout << maxleftA << " " << minrightA << " " << maxleftB << " " << minrightB << endl;
 
     // check condition for medians
-    if (maxleftX <= minrightY && maxleftY <= minrightX) {
-      return N & 1 ? max(maxleftX, maxleftY) :
-                    (max(maxleftX, maxleftY) + min(minrightY, minrightX)) / 2.0;
-    } else if (maxleftX > minrightY) {
-      cout << "move right bound" << endl;
-      // move left-
-      right = partX - 1;
+    if (maxleftA <= minrightB && maxleftB <= minrightA) {
+      return N & 1 ? max(maxleftA, maxleftB) :
+                    (max(maxleftA, maxleftB) + min(minrightB, minrightA)) / 2.0;
+    } else if (maxleftA > minrightB) {
+      // move right bound
+      right = ma - 1;
     } else {
-      cout << "move left bound" << endl;
-      // move right
-      left = partX + 1;
+      // move left bound
+      left = ma + 1;
     }
   }
   return 0.;
