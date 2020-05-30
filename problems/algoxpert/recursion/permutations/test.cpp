06df7e8f9345cf927f1f326ee2fcf136bbc3f6f7
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-05-30 09:02:58 +0100

    added one more solution to the permuattions problem

--- problems/algoxpert/recursion/permutations/test.cpp
+++ problems/algoxpert/recursion/permutations/test.cpp
@@ -3,6 +3,32 @@
 #include <algorithm>
 using namespace std;
 
+/* Solution 1
+ * The gist here is that every number can be at every position and 
+ * the rest of the numbers in any given permutation are the numbers 
+ * which are NOT the current number;
+ * */
+void solve2(vector<vector<int>>& res, vector<int> current, vector<int>& a) {
+  if (a.size() == 0) {
+    res.push_back(current);
+  } else {
+    for (int i=0; i<a.size(); ++i) {
+      int c = a[i];
+      auto rest = [&a, &c]() {
+        vector<int> res;
+        res.reserve(a.size()-1);
+        for (int j=0; j<a.size(); ++j) {
+          if (a[j] != c) {
+            res.push_back(a[j]);
+          }
+        }
+        return res;
+      }();
+      solve2(res, current, rest);
+    }
+  }
+}
+
 // Solution 2 
 void solve(vector<vector<int>>& res, int i, vector<int>& a) {
   if (i == a.size()-1) {
@@ -21,7 +47,8 @@ vector<vector<int>> getPermutations(vector<int>& a) {
     return {};
   }
   vector<vector<int>> res;
-  solve(res, 0, a);
+  //solve(res, 0, a);
+  solve(res, {}, a);
   return res;
 }
 
