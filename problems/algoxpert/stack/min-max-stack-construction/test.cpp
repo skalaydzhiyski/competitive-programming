7cd74cd06a717be9e7a36afeaacc482fe9f9e24d
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-07-25 15:33:41 +0100

    added min max stack solutionnN

--- problems/algoxpert/stack/min-max-stack-construction/test.cpp
+++ problems/algoxpert/stack/min-max-stack-construction/test.cpp
@@ -1,7 +1,55 @@
-#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+using Node = unordered_map<string, int>;
+
+class MinMaxStack {
+  vector<Node> stack;
+public:
+  int peek() {
+    return stack[stack.size()-1]["value"];
+  }
+
+  int pop() {
+    int res = stack.back()["value"];
+    stack.pop_back();
+    return res;
+  }
+
+  void push(int number) {
+    if (stack.empty()) {
+      Node node {
+        {"value", number}, {"min", number}, {"max", number}
+      };
+      stack.push_back(node);
+      return;
+    }
+    Node top = stack[stack.size()-1];
+    Node node {{"value", number}};
+    node["max"] = max(number, top["max"]);
+    node["min"] = min(number, top["min"]);
+    stack.push_back(node);
+  }
+
+  int getMin() {
+    return stack[stack.size()-1]["min"];
+  }
+
+  int getMax() {
+    return stack[stack.size()-1]["max"];
+  }
+};
 
 int main(void) {
-  // write your solution here
+  MinMaxStack stack;
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
   return 0;
 }
+
 
