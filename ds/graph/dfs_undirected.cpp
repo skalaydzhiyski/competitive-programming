426be8194ab99257314abc4f1aaef5d08df06b3a
Spas Kalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2020-07-30 16:35:42 +0100

    added dfs and bfs implementations in ./ds/graph

---
+++ ds/graph/dfs_undirected.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(const vector<vector<int>>& g, vector<bool>& visited, int s) {
+  cout << s << ",";
+  visited[s] = 1;
+  for (const auto& c: g[s]) {
+    if (!visited[c]) {
+      dfs(g, visited, c);
+    }
+  }
+}
+
+int main(void) {
+  int N = 10;
+  vector<bool> visited(N, 0);
+  vector<vector<int>> graph(N, vector<int>{});
+
+  vector<pair<int, int>> edges {
+    {0,1},{1,2},{1,3},{1,4},{2,5},{2,6},{3,8},{3,9},{6,7}
+  };
+  for (auto e: edges) {
+    graph[e.first].push_back(e.second);
+    graph[e.second].push_back(e.first);
+  }
+
+  for (int i=0; i<N; ++i) {
+    cout << i << ": ";
+    for (auto node: graph[i]) {
+      cout << node << ",";
+    }
+    cout << endl;
+  }
+  cout << "---------------- DFS -----------------" << endl;
+  dfs(graph, visited, 0);
+  cout << endl;
+  return 0;
+}
