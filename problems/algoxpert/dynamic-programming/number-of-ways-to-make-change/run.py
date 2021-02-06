9f1649e70dcb0be03266bc4bcd3e9d086b62d850
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-02-06 12:13:55 +0000

    added max-subset-no-adjacent solution

--- problems/algoxpert/dynamic-programming/number-of-ways-to-make-change/run.py
+++ problems/algoxpert/dynamic-programming/number-of-ways-to-make-change/run.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 
+def solve(a, t):
+  dp = [1] + [0]*t
+  for x in a:
+    for i,d in enumerate(dp):
+      if x <= i:
+        dp[i] += dp[i-x]
+  return dp[-1]
+
+
+def numberOfWaysToMakeChange(t, a):
+  return solve(a, t)
+
+t = 10
+a = [1, 5, 10, 25]
+print(numberOfWaysToMakeChange(t, a))
+
