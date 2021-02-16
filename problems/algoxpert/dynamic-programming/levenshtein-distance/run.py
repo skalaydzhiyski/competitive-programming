554f6bf9c8d763a4f56baf7bb633441c412c38f0
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-02-16 07:51:40 +0000

    first solution added

---
+++ problems/algoxpert/dynamic-programming/levenshtein-distance/run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+def show(dp):
+  for x in dp:
+    print(x)
+  print("-"*20)
+
+
+def solve(s1, s2):
+  size1, size2 = len(s1)+1, len(s2)+1
+  dp = [[x for x in range(size2)]]
+  dp += [[x] + [0]*(size2-1) for x in range(1,size1)]
+  for i in range(1, size1):
+    for j in range(1, size2):
+      diag, left, top = dp[i-1][j-1], dp[i][j-1], dp[i-1][j]
+      dp[i][j] = min([diag, left, top])+1 if s1[i-1] != s2[j-1] else diag
+  #show(dp)
+  return dp[-1][-1]
+
+
+def levenshteinDistance(str1, str2):
+  return solve(str1, str2)
+
+
+s1, s2 = 'abc', 'yabd'
+print(levenshteinDistance(s1, s2))
+
+s1, s2 = 'biting', 'mitten'
+#print(levenshteinDistance(s1, s2))
+
+
