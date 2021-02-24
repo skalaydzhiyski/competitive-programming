291b7dff264a332a68a9f0c640540e01a9ead828
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-02-24 08:16:25 +0000

    added timings to both approaches. it seems they're almost exactly the same

--- problems/algoxpert/graph/traverse-iter/run.py
+++ problems/algoxpert/graph/traverse-iter/run.py
@@ -16,9 +16,7 @@ def traverse(g, start, algo='dfs'):
   while len(to_visit):
     current = to_visit.pop() if algo == 'dfs' else to_visit.pop(0)
     res.append(current)
-    neigh = g[current] if algo == 'bfs' else list(reversed(g[current]))
-    for node in neigh:
-      if node not in res:
+    for node in g[current]:
         to_visit.append(node)
     #log(res, to_visit, current)
   return res
@@ -51,18 +49,28 @@ g = {
     8: [],
     9: [],
 }
+t0 = time()
 iter_dfs = traverse(g,1)
+print(f'time iter dfs: {time() - t0}')
 print(iter_dfs)
+
+t0 = time()
 rec_dfs = []
 _dfs(g, 1, rec_dfs)
+print(f'time rec dfs: {time() - t0}')
 print(rec_dfs)
 print(rec_dfs == iter_dfs)
 
 # bfs test
+t0 = time()
 iter_bfs = traverse(g, 1, 'bfs')
+print(f'\ntime iter bfs: {time() - t0}')
+
 print(iter_bfs)
+t0 = time()
 res = []
 _bfs(g, 1, [], res)
+print(f'time rec bfs: {time() - t0}')
 print(res)
 print(res == iter_bfs)
 
