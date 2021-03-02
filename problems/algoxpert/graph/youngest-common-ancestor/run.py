80708498c722480b586308bb3f9f2369954a4bcd
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-03-02 08:47:01 +0000

    youngest-common-ancestor solution added

--- problems/algoxpert/graph/youngest-common-ancestor/run.py
+++ problems/algoxpert/graph/youngest-common-ancestor/run.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-import time
 
 
 # This is an input class. Do not edit.
 class AncestralTree:
   def __init__(self, name):
-    self.name = name
+    self.name = name 
     self.ancestor = None
 
-def solve(root, d1, d2):
-  # write your solution here
-  pass
+
+def height(node, root):
+  res = 0
+  current = node
+  while current.name != root.name:
+    res += 1
+    current = current.ancestor
+  return res
+
+
+def solve(root, node1, node2):
+  h1 = height(node1, root)
+  h2 = height(node2, root)
+  if h1 > h2:
+    return backtrack(node1, node2, h1-h2)
+  else:
+    return backtrack(node2, node1, h2-h1)
+
+
+def backtrack(low, high, diff):
+  while diff > 0:
+    low = low.ancestor
+    diff -=1
+  while low != high:
+    low = low.ancestor
+    high = high.ancestor
+  return high
 
 
 def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
-  # Write your code here.
-  pass
+  return solve(topAncestor, descendantOne, descendantTwo)
 
 
 
