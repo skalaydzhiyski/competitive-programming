5999707a5de1b26ea7f6b26a2487d89f4dbdc95d
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-10-22 10:20:12 +0100

    add 1089 solution

---
+++ leetcode/1089.duplicate-zeros.py
@@ -0,0 +1,29 @@
+class Solution:
+  def duplicateZeros(self, arr):
+    """
+    Do not return anything, modify arr in-place instead.
+    """
+    N = len(arr)
+    res = []
+    l = 0
+    i = 0
+    while l < N:
+      if arr[i] != 0:
+        res += [arr[i]]
+        l += 1
+      else:
+        res += [arr[i], arr[i]]
+        l += 2
+      i += 1
+    for i in range(N):
+	    arr[i] = res[i]
+
+
+
+arr = [1,0,2,3,0,4,5,0]
+res = Solution().duplicateZeros(arr)
+print(res)
+
+arr = [1,0,2,3]
+res = Solution().duplicateZeros(arr)
+print(res)
