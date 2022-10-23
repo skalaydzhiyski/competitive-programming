aa00c720bfa9b7428a54e59a1dba3b8255457a41
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-10-23 12:15:06 +0100

    add inplace solution to 27 with O(1) memory

--- leetcode/27.remove-element.py
+++ leetcode/27.remove-element.py
@@ -1,11 +1,15 @@
 class Solution:
-  def removeElement(self, nums, val):
-    res = []
-    k = 0
-    for n in nums:
-      if n != val:
-        res.append(n)
-        k += 1
-    for i in range(k):
-      nums[i] = res[i]
-    return k
+  def removeDuplicates(self, nums: List[int]) -> int:
+    N = len(nums)
+    if N == 1: return 1
+    res = 1
+    for i in range(N-1):
+      j = i+1
+      if nums[i] >= nums[j]:
+        k = j
+        while k < N and nums[i] >= nums[k]:
+          k += 1
+        if k == N: break
+        nums[j] = nums[k]
+      res += 1
+    return res
