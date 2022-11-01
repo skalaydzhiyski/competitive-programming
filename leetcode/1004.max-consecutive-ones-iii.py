2538b291941ee74ddef4573e26fb8e366487280d
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-01 14:26:01 +0000

    add one more solution with proper sliding window for 1004

--- leetcode/1004.max-consecutive-ones-iii.py
+++ leetcode/1004.max-consecutive-ones-iii.py
@@ -1,12 +1,27 @@
-from multiprocessing import Queue
+# SLIDING WINDOW SOLUTION (O(N) time; O(1) space)
 class Solution:
+    def longestOnes(self, nums, k):
+        N = len(nums)
+        left = 0
+        current = res = 0
+        for right in range(N):
+            if nums[right] == 0:
+                k -= 1
+            if k < 0:
+                if nums[left] == 0:
+                    k += 1
+                left += 1
+        return right - left + 1
+
+# QUEUE SOLUTION (O(N) time; O(N) space)
+from multiprocessing import Queue
+class Solution2:
     def longestOnes(self, nums, k):
         N = len(nums)
         zeros = Queue()
         for i in range(N):
             if nums[i] == 0: zeros.put(i)
-
-        res, current = 0, 0
+        current = res = 0
         for i in range(N):
             if nums[i] == 1:
                 current += 1
@@ -21,7 +36,7 @@ class Solution:
         return res
 
 
-nums = [1,1,1,0,0,0,1,1,1,1,0]
-k = 2
-res = Solution().longestOnes(nums, k)
+nums = [1,0,0,1,1,0,1]
+k = 1
+res = Solution2().longestOnes(nums, k)
 print(res)
