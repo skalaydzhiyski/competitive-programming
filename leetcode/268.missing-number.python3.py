a7d1ff423f6d16d23a32ad721bc571bb534fa253
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-02 15:39:38 +0000

    add bitwise and mathy solutions to 268

--- leetcode/268.missing-number.python3.py
+++ leetcode/268.missing-number.python3.py
@@ -1,4 +1,22 @@
+# Gauss formula for summation
 class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected = (n*(n+1))//2
+        actual = sum(nums)
+        return expected - actual
+
+# Bit manipulation
+class Solution2:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)+1): res ^= i
+        for x in nums: res ^= x
+        return res
+
+
+# Mark every seen number with a string (because we have a 0 we cannot perform the trick with flipping the sign.)
+class Solution3:
     def missingNumber(self, nums: List[int]) -> int:
         N = len(nums)
         nums.append(1)
