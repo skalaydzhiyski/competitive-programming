c5d847194a182557014280488131cad7b91824e6
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-02 11:47:51 +0000

    refactor 1004 and remove queue from personal solution

--- leetcode/1004.max-consecutive-ones-iii.py
+++ leetcode/1004.max-consecutive-ones-iii.py
@@ -1,10 +1,30 @@
-# SLIDING WINDOW SOLUTION (O(N) time; O(1) space)
+# SAME AS QUEUE BUT WITH LIST
 class Solution:
     def longestOnes(self, nums, k):
         N = len(nums)
-        left = 0
+        zi, zeros = 0, [i for i in range(N) if nums[i] == 0]
         current = res = 0
+        for i in range(N):
+            if nums[i] == 1:
+                current += 1
+            else:
+                if k > 0:
+                    current += 1
+                    k -= 1
+                else:
+                    current = i - zeros[zi]
+                    zi += 1
+            if current > res:
+                res = current
+        return res
+
+# SLIDING WINDOW SOLUTION (O(N) time; O(1) space) (Optimal)
+class Solution3:
+    def longestOnes(self, nums, k):
+        N = len(nums)
+        left = 0
         for right in range(N):
+            print(left, right)
             if nums[right] == 0:
                 k -= 1
             if k < 0:
@@ -36,7 +56,7 @@ class Solution2:
         return res
 
 
-nums = [1,0,0,1,1,0,1]
+nums = [1,0,0,1,0,1,0,0]
 k = 1
-res = Solution2().longestOnes(nums, k)
+res = Solution3().longestOnes(nums, k)
 print(res)
