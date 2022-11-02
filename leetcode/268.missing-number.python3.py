c61b624496f456b19e0ef3ef0b928c841460e59d
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-02 15:42:02 +0000

    reduced one line from the main solution for 268

--- leetcode/268.missing-number.python3.py
+++ leetcode/268.missing-number.python3.py
@@ -3,8 +3,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         expected = (n*(n+1))//2
-        actual = sum(nums)
-        return expected - actual
+        return expected - sum(nums)
 
 # Bit manipulation
 class Solution2:
