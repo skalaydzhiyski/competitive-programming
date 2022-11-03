3a31544d6f5b506cb0b8498bc0104e871ddca5b9
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-03 11:40:28 +0000

    one liner for counting

--- leetcode/1133.largest-unique-number.python3.py
+++ leetcode/1133.largest-unique-number.python3.py
@@ -1,7 +1,6 @@
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
         store = defaultdict(int)
-        for x in nums:
-            store[x] += 1
+        for x in nums: store[x] += 1
         store = [k for k,v in store.items() if v == 1]
         return -1 if not len(store) else max(store)
