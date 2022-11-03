5aa07de686abcdecf0a696ef501dd9c658e3bafa
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-03 11:40:07 +0000

    add solution for 1133

---
+++ leetcode/1133.largest-unique-number.python3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        store = defaultdict(int)
+        for x in nums:
+            store[x] += 1
+        store = [k for k,v in store.items() if v == 1]
+        return -1 if not len(store) else max(store)
