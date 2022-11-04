8d2f2a779d83571c6fc67bfb934eb745624dc087
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-04 15:16:35 +0000

    4 indentation to be accepted in leetcode UI

--- leetcode/3.longest-substring-without-repeating-characters.py
+++ leetcode/3.longest-substring-without-repeating-characters.py
@@ -1,20 +1,20 @@
 class Solution:
-  def lengthOfLongestSubstring(self, s):
-    n = len(s)
-    if n == 0: return 0
-    res, count = 1,1
-    i, j = 0, 1
-    m = {}
-    m[s[i]] = 1
-    while j<n:
-      if s[j] not in m: 
-        m[s[j]] = 1
-        count += 1
-        j += 1
-      else:
-        i += 1
-        j = i+1
-        count = 1
-        m = {s[i]:1}
-      res = max(res, count)
-    return res
+    def lengthOfLongestSubstring(self, s):
+      n = len(s)
+      if n == 0: return 0
+      res, count = 1,1
+      i, j = 0, 1
+      m = {}
+      m[s[i]] = 1
+      while j<n:
+          if s[j] not in m: 
+              m[s[j]] = 1
+              count += 1
+              j += 1
+          else:
+              i += 1
+              j = i+1
+              count = 1
+              m = {s[i]:1}
+          res = max(res, count)
+      return res
