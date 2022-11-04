fc9f7f5b8e33a211c12e4bda00e19ae0b224f931
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2022-11-04 15:11:35 +0000

    one liner solution for 771

--- leetcode/771.jewels-and-stones.python3.py
+++ leetcode/771.jewels-and-stones.python3.py
@@ -1,4 +1,3 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        store = set(jewels)
-        return sum([x in store for x in stones])        
+        return sum([x in set(jewels) for x in stones])        
