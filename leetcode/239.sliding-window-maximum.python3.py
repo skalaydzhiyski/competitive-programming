f2a17f8539181de29b03b3656be40a348e3a69fa
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-16 13:08:42 +0000

    solution from scratch for 239

--- leetcode/239.sliding-window-maximum.python3.py
+++ leetcode/239.sliding-window-maximum.python3.py
@@ -1,18 +1,20 @@
 from collections import deque
+from typing import List
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         N = len(nums)
         q = deque()
         res = []
-        for idx in range(N):
-            while q and nums[idx] > nums[q[-1]]:
-                q.pop()
-
-            q.append(idx)
-            
-            if q[0] + k == idx:
+        for current in range(N):
+            # maintain monotonic decreasing property
+            while q and nums[current] >= nums[q[-1]]:
+                _ = q.pop()
+            q.append(current)
+            # if the current max is off window -> remove
+            if q[0] + k <= current:
                 q.popleft()
-
-            if idx >= k-1:
+            # accumulate result list
+            if current >= k-1:
                 res.append(nums[q[0]])
         return res
