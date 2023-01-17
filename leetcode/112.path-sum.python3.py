34efd0e5535c620f3f563b6c57268dbdae41e037
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-17 08:56:49 +0000

    add iterative solution for 112

--- leetcode/112.path-sum.python3.py
+++ leetcode/112.path-sum.python3.py
@@ -1,15 +1,35 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# racursive (prefer this for trees!)
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if root is None:
             return False
+
+        targetSum -= root.val
         if root.left is None and root.right is None:
-            return root.val - targetSum == 0
-        hasleft  = self.hasPathSum(root.left, targetSum)
-        hasright = self.hasPathSum(root.right, targetSum)
-        return hasleft or hasright
+            return targetSum == 0
+
+        has_left  = self.hasPathSum(root.left, targetSum)
+        has_right = self.hasPathSum(root.right, targetSum)
+        return has_left or has_right
+
+# iterative
+class SolutionIter:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+        stack = [(root, 0)]
+        while stack:
+            current, target = stack.pop()
+
+            target += current.val
+            if current.left is None and current.right is None:
+                if target == targetSum:
+                    return True
+
+            if current.left is not None:
+                stack.append((current.left, target))
+            if current.right is not None:
+                stack.append((current.right, target))
+
+        return False
