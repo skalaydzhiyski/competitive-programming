b1f83541ff1d320397daa69dbcf888af2810ec77
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-23 14:28:15 +0000

    add iterative solution to 270

--- leetcode/270.closest-binary-search-tree-value.python3.py
+++ leetcode/270.closest-binary-search-tree-value.python3.py
@@ -4,7 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        res = root.val
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            if abs(current.val - target) < abs(res - target):
+                res = current.val
+            if current.left and target < current.val:
+                stack.append(current.left)
+            elif current.right and target > current.val:
+                stack.append(current.right)
+        return res
+
+
+class SolutionRec:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
         def solve(root, target, res):
             if root is None:
