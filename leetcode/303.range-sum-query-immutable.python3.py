e7dc1f58f8908197e21f7bdd2ef829053f778544
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-23 16:23:20 +0000

    no maxsize for cache

--- leetcode/303.range-sum-query-immutable.python3.py
+++ leetcode/303.range-sum-query-immutable.python3.py
@@ -4,7 +4,7 @@ class NumArray:
     def __init__(self, nums: List[int]):
         self.nums = nums
 
-    @lru_cache
+    @lru_cache(maxsize=None)
     def sumRange(self, left: int, right: int) -> int:
         return sum(self.nums[left:right+1])
 
