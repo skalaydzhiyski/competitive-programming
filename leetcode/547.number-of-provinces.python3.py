673c6faac004ff36138f7ce53a844e28431032f4
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-24 10:50:09 +0000

    add solution for 547

--- leetcode/547.number-of-provinces.python3.py
+++ leetcode/547.number-of-provinces.python3.py
@@ -1,4 +1,22 @@
+from collections import defaultdict
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # TODO: continue here when you're back to graphs.
-        pass        
+        N = len(isConnected)
+        visited = [0] * N
+        m = {node: [] for node in range(N)}
+        for i in range(N):
+            for j in range(i+1, N):
+                if isConnected[i][j]:
+                    m[i].append(j)
+                    m[j].append(i)
+        res = 0
+        for node in range(N):
+            if visited[node]: continue
+            to_visit = [node]
+            while to_visit:
+                current = to_visit.pop()
+                visited[current] = 1
+                to_visit += [x for x in m[current] if not visited[x]]
+            res += 1
+        return res
