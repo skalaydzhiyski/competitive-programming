f6f39221f291b27a616253b947383d20b3ab4abd
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-26 08:11:55 +0000

    iterative solution

--- leetcode/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.python3.py
+++ leetcode/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.python3.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        self.res = 0
         graph = defaultdict(set)
         paths = set()
         for left, right in connections:
@@ -10,15 +9,17 @@ class Solution:
             graph[right].add(left)
             paths.add( (left, right) )
 
+        res = 0
         visited = set()
-        def dfs(current, parent):
-            if current != 0 and (current, parent) not in paths:
-                self.res += 1
-                
+        to_visit = [(0, -1)]
+        while to_visit:
+            current, parent = to_visit.pop()
             visited.add(current)
-            for node in graph[current]:
-                if node not in visited:
-                    dfs(node, current)
-
-        dfs(0, -1)
-        return self.res
+            if current != 0 and (current, parent) not in paths:
+                res += 1
+            to_visit += [
+                (node, current) 
+                for node in graph[current]
+                if node not in visited
+            ]
+        return res
