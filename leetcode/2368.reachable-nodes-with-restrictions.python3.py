eced1ca45c7bb07f5744ca77a52e5296514b037f
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-01-31 20:21:09 +0000

    add solution to 283 .. previously solved

---
+++ leetcode/2368.reachable-nodes-with-restrictions.python3.py
@@ -0,0 +1,4 @@
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        # TODO: continue here when you're coming back graphs.
+        pass
