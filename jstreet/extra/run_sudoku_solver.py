504ab7e6074a1a069d3031a2efc733d5f64e8c43
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-02-02 11:46:40 +0000

    add solution to sudoku solve with backtracking

--- jstreet/extra/run_sudoku_solver.py
+++ jstreet/extra/run_sudoku_solver.py
@@ -1,9 +1,26 @@
 import numpy as np
 from sudoku import Sudoku
 
-def solve():
-  pass
+def can_put_number_at(x, y, n):
+    global board
+    row = board[x]
+    col = board[:, y]
 
-board = np.matrix(Sudoku(3).difficulty(.5).board)
+def solve():
+    global board, rows, cols
+    for x in range(rows):
+        for y in range(cols):
+            for n in range(1,10):
+                if can_put_number_at(x, y, n):
+                    board[x][y] = n
+                    solve()
+                    board[x][y] = 0
+            return
+    return board
+
+board = np.array(Sudoku(3).difficulty(.5).board)
 board[board == None] = 0
-print(board)
+rows, cols = board.shape
+print(f'Board:\n{board}')
+res = solve()
+print(f'Solution:\n{res}')
