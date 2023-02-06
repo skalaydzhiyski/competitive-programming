d287bbd8422c1df0a5471c06da52eb7de5fe7479
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-02-06 15:37:10 +0000

    add solution to 542

--- leetcode/542.01-matrix.python3.py
+++ leetcode/542.01-matrix.python3.py
@@ -2,49 +2,26 @@ from collections import deque
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        def get_neighbours_for(node):
-            i,j = node
-            directions = (1,0), (-1,0), (0,1), (0,-1)
-            neighbours = [(i+di, j+dj) for di,dj in directions]
-            return [
-                (x,y)
-                for x,y in neighbours
-                if 0 <= x < m and 0 <= y < n
-            ]
-
-        res = mat.copy()
-        ones = set()
         m,n = len(mat), len(mat[0])
+        visited = set()
+        to_visit = deque()
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 1:
-                    ones.add((i,j))
-                    res[i][j] = None
+                if mat[i][j] == 0:
+                    visited.add((i,j))
+                    to_visit.append( ((i,j), 0) )
 
-        visited = set()
-        for one in ones:
-            if one in visited:
-                continue
-
-            to_visit = deque([one])
-            while to_visit:
-                node = to_visit.popleft()
-                visited.add(node)
-                neighbours = get_neighbours_for(node)
-
-                if any([mat[x][y] == 0 for x,y in neighbours]):
-                    res[node[0]][node[1]] = 1
-                    continue
-
-                seen = []
-                for x,y in neighbours:
-                    if res[x][y]:
-                        seen.append(mat[x][y])
-                if len(seen):
-                    res[node[0]][node[1]] = min(seen) + 1
-                    continue
-
-                to_visit += [n for n in neighbours if n not in visited]
-                to_visit.append(node)
-                
-        return res
+        directions = (1,0), (-1,0), (0,1), (0,-1)
+        valid = lambda x,y: 0 <= x < m and 0 <= y <n
+        while to_visit:
+            (i, j), distance = to_visit.popleft()
+            for di, dj in directions:
+                next_node = (i + di, j + dj)
+                if next_node not in visited and valid(*next_node):
+                    visited.add(next_node)
+                    distance_to_next = distance + 1
+                    to_visit.append(
+                        (next_node, distance_to_next)
+                    )
+                    mat[next_node[0]][next_node[1]] = distance_to_next
+        return mat
