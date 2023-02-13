c786a1aa7eb6c5e5b08a704b55869711d789b6bc
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-02-13 14:14:28 +0000

    latest

---
+++ jstreet/extra/june_2018_puzzle.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+grid = np.array([
+	[0,4,0,0,0,0,0],
+	[0,0,6,3,0,0,6],
+	[0,0,0,0,0,5,5],
+	[0,0,0,4,0,0,0],
+	[4,7,0,0,0,0,0],
+	[2,0,0,7,4,0,0],
+	[0,0,0,0,0,1,0]
+])
+
+# NOTE: the valid numbers for the square will have to be recomputed
+#       every time we add a number through the main logic.
