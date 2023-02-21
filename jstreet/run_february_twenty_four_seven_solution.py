13b39eee0cf0a6dd6ebcca3ee2d35ce4d7abf04d
Spas Kalaydzhiyski <spas.kalaydzhiyski@santander.co.uk>
2023-02-21 13:44:14 +0000

    latest

--- jstreet/run_february_twenty_four_seven_solution.py
+++ jstreet/run_february_twenty_four_seven_solution.py
@@ -1,3 +1,14 @@
 #!/Users/darchitect/miniconda3/bin/python3
 import numpy as np
 
+board = np.array([
+    [0,4,0,0,0,0,0],
+    [0,0,6,3,0,0,6],
+    [0,0,0,0,0,5,5],
+    [0,0,0,4,0,0,0],
+    [4,7,0,0,0,0,0],
+    [2,0,0,7,4,0,0],
+    [0,0,0,0,0,1,0],
+])
+print(board)
+
