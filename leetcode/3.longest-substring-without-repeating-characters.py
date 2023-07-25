fd7a6aaec9765c8dda686b7658b0af1e0633e316
e1211913 <e1211913-sanuk_sangrou>
2023-07-25 10:43:44 +0100

    lastest

--- leetcode/3.longest-substring-without-repeating-characters.py
+++ leetcode/3.longest-substring-without-repeating-characters.py
@@ -1,20 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
-      n = len(s)
       if n == 0: return 0
+      n = len(s)
       res, count = 1,1
       i, j = 0, 1
-      m = {}
-      m[s[i]] = 1
+      m = set(m[i])
       while j<n:
-          if s[j] not in m: 
-              m[s[j]] = 1
+          if s[j] not in m:
+              m.add(s[j])
               count += 1
               j += 1
           else:
               i += 1
               j = i+1
               count = 1
-              m = {s[i]:1}
+              m = set(s[i])
           res = max(res, count)
       return res
--- leetcode/599.minimum-index-sum-of-two-lists.python3.py
+++ leetcode/599.minimum-index-sum-of-two-lists.python3.py
@@ -1,10 +1,23 @@
 from collections import defaultdict
 
 class Solution:
-    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+    def findRestaurant(self, list1, list2):
             res = defaultdict(list)
             for i in range(len(list1)):
                 for j in range(len(list2)):
                     if list1[i] == list2[j]:
                         res[(i+j)].append(list1[i])
             return res[min(res.keys())]
+
+
+list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+print(Solution().findRestaurant(list1, list2))
+
+list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+list2 = ["KFC","Shogun","Burger King"]
+print(Solution().findRestaurant(list1, list2))
+
+list1 = ["happy","sad","good"]
+list2 = ["sad","happy","good"]
+print(Solution().findRestaurant(list1, list2))
